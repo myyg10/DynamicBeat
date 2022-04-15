dd17aca0a96899276e7cd30e966589c96718d012
myyg10 <myyg10@naver.com>
2022-04-15 23:56:22 +0900

    인트로 음악 작업 완료

---
+++ src/dynamic_beat_3/DynamicBeat.java
@@ -0,0 +1,41 @@
+package dynamic_beat_3;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class DynamicBeat extends JFrame {
+
+	private Image screenImage;
+	private Graphics screenGraphics;
+	
+	private Image introBackground;
+	
+	public DynamicBeat() {
+		setTitle("Dynamic Beat");
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		setResizable(false); //사이즈 고정
+		setLocationRelativeTo(null); //화면 가운데 출력
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //종료 버튼
+		setVisible(true);
+		
+		introBackground = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
+		
+		Music introMusic = new Music("introMusic.mp3", true);
+		introMusic.start();
+	}
+	
+	public void paint(Graphics g) {
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		screenGraphics = screenImage.getGraphics();
+		screenDraw(screenGraphics);
+		g.drawImage(screenImage, 0, 0, null);
+	}
+	
+	public void screenDraw(Graphics g) {
+		g.drawImage(introBackground, 0, 0, null);
+		this.repaint();
+	}
+}
